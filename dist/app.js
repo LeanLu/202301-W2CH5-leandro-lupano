1bd5ef09995f9e44d3d3e2f09cfac93a8c8a5b34
Leon <leandrolupano@hotmail.com>
2023-01-28 16:33:27 +0100

    Add check and change status function for no border cells

--- dist/app.js
+++ dist/app.js
@@ -1,51 +1,12 @@
-"use strict";
+import { checkAroundNoBorder } from './checkAroundFunction.js';
 const linesQuantities = 3;
 const columnsQuantities = 3;
 const cells = [
     [0, 1, 0],
-    [0, 0, 1],
-    [1, 1, 1],
+    [0, 1, 1],
+    [0, 1, 1],
 ];
 console.table(cells);
-/*
-const checkAround = (array: number[][]) => {
-  for (let i = 0; i < columnsQuantities; i++) {
-    for (let j = 0; j < linesQuantities; j++) {
-      let quantityOfAlive = 0;
-
-      // Ejemplo: i= 1; j = 1;
-
-      if (array[i - 1][j - 1] === 1) quantityOfAlive++;
-
-      if (array[i - 1][j] === 1) quantityOfAlive++;
-
-      if (array[i - 1][j + 1] === 1) quantityOfAlive++;
-
-      if (array[i][j - 1] === 1) quantityOfAlive++;
-
-      if (array[i][j + 1] === 1) quantityOfAlive++;
-
-      if (array[i + 1][j - 1] === 1) quantityOfAlive++;
-
-      if (array[i + 1][j] === 1) quantityOfAlive++;
-
-      if (array[i + 1][j + 1] === 1) quantityOfAlive++;
-
-      if (quantityOfAlive < 2) array[i][j] = 0;
-      if (quantityOfAlive > 3) array[i][j] = 0;
-      if (quantityOfAlive === 2 || quantityOfAlive === 3) array[i][j] = 1;
-    }
-  }
-};
-*/
-for (let i = 0; i < cells.length; i++) {
-    for (let j = 0; j < cells.length; j++) {
-        if (cells[i][j] === 0) {
-            cells[i][j] = 1;
-        }
-        else {
-            cells[i][j] = 0;
-        }
-    }
-}
+//checkAroundBorder(cells, columnsQuantities, linesQuantities);
+checkAroundNoBorder(cells, columnsQuantities, linesQuantities);
 console.table(cells);
