3349093e35a87c60ebf6e55aa2f14564ae9ff236
Leon <leandrolupano@hotmail.com>
2023-01-29 21:35:23 +0100

    Add Interval to start function

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,7 @@
 import { checkAround } from './checkAround.js';
 const linesQuantities = 5;
 const columnsQuantities = 5;
-const cells = [
+let cells = [
     [0, 1, 0, 0, 0],
     [0, 0, 1, 0, 0],
     [1, 1, 1, 0, 0],
@@ -9,48 +9,8 @@ const cells = [
     [0, 0, 0, 0, 0],
 ];
 console.table(cells);
-const newCellsTable = checkAround(cells, columnsQuantities, linesQuantities);
-console.table(newCellsTable);
-const newCellsTable2 = checkAround(newCellsTable, columnsQuantities, linesQuantities);
-console.table(newCellsTable2);
-const newCellsTable3 = checkAround(newCellsTable2, columnsQuantities, linesQuantities);
-console.table(newCellsTable3);
-const newCellsTable4 = checkAround(newCellsTable3, columnsQuantities, linesQuantities);
-console.table(newCellsTable4);
-const newCellsTable5 = checkAround(newCellsTable4, columnsQuantities, linesQuantities);
-console.table(newCellsTable5);
-const newCellsTable6 = checkAround(newCellsTable5, columnsQuantities, linesQuantities);
-console.table(newCellsTable6);
-const newCellsTable7 = checkAround(newCellsTable6, columnsQuantities, linesQuantities);
-console.table(newCellsTable7);
-const newCellsTable8 = checkAround(newCellsTable7, columnsQuantities, linesQuantities);
-console.table(newCellsTable8);
-const newCellsTable9 = checkAround(newCellsTable8, columnsQuantities, linesQuantities);
-console.table(newCellsTable9);
-/* PRUEBA DE SET INTERVAL:
-let newCellsArray: number[][] = cells;
-
-const startGame = (
-  array: number[][],
-  columnsQuantities: number,
-  linesQuantities: number
-) => {
-  newCellsArray = checkAround(array, columnsQuantities, linesQuantities);
-  debugger;
-  console.table(newCellsArray);
+const startGame = () => {
+    cells = checkAround(cells, columnsQuantities, linesQuantities);
+    console.table(cells);
 };
-startGame(newCellsArray, columnsQuantities, linesQuantities);
-
 setInterval(startGame, 1000);
-*/
-/*
-const arrayTest = [
-  [1, 1, 1],
-  [1, 1, 0],
-  [0, 0, 0],
-];
-
-const cellTest = checkAround(arrayTest, 3, 3);
-
-console.table(cellTest);
-*/
--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { checkAround } from './checkAround.js';
 const linesQuantities: number = 5;
 const columnsQuantities: number = 5;
 
-const cells: number[][] = [
+let cells: number[][] = [
   [0, 1, 0, 0, 0],
   [0, 0, 1, 0, 0],
   [1, 1, 1, 0, 0],
@@ -13,90 +13,9 @@ const cells: number[][] = [
 
 console.table(cells);
 
-const newCellsTable = checkAround(cells, columnsQuantities, linesQuantities);
-console.table(newCellsTable);
-
-const newCellsTable2 = checkAround(
-  newCellsTable,
-  columnsQuantities,
-  linesQuantities
-);
-console.table(newCellsTable2);
-
-const newCellsTable3 = checkAround(
-  newCellsTable2,
-  columnsQuantities,
-  linesQuantities
-);
-console.table(newCellsTable3);
-
-const newCellsTable4 = checkAround(
-  newCellsTable3,
-  columnsQuantities,
-  linesQuantities
-);
-console.table(newCellsTable4);
-
-const newCellsTable5 = checkAround(
-  newCellsTable4,
-  columnsQuantities,
-  linesQuantities
-);
-console.table(newCellsTable5);
-
-const newCellsTable6 = checkAround(
-  newCellsTable5,
-  columnsQuantities,
-  linesQuantities
-);
-console.table(newCellsTable6);
-
-const newCellsTable7 = checkAround(
-  newCellsTable6,
-  columnsQuantities,
-  linesQuantities
-);
-console.table(newCellsTable7);
-
-const newCellsTable8 = checkAround(
-  newCellsTable7,
-  columnsQuantities,
-  linesQuantities
-);
-console.table(newCellsTable8);
-
-const newCellsTable9 = checkAround(
-  newCellsTable8,
-  columnsQuantities,
-  linesQuantities
-);
-console.table(newCellsTable9);
-
-/* PRUEBA DE SET INTERVAL:
-let newCellsArray: number[][] = cells;
-
-const startGame = (
-  array: number[][],
-  columnsQuantities: number,
-  linesQuantities: number
-) => {
-  newCellsArray = checkAround(array, columnsQuantities, linesQuantities);
-  debugger;
-  console.table(newCellsArray);
+const startGame = () => {
+  cells = checkAround(cells, columnsQuantities, linesQuantities);
+  console.table(cells);
 };
-startGame(newCellsArray, columnsQuantities, linesQuantities);
 
 setInterval(startGame, 1000);
-*/
-
-/*
-const arrayTest = [
-  [1, 1, 1],
-  [1, 1, 0],
-  [0, 0, 0],
-];
-
-const cellTest = checkAround(arrayTest, 3, 3);
-
-console.table(cellTest);
-*/
